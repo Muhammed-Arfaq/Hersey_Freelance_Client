2c9d051e47b8133d1cecc00cf562fbc992393709
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-03-03 15:50:53 +0530

    updated

--- src/axios.jsx
+++ src/axios.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 const instance = axios.create({
     // baseURL: "http://13.233.255.36/"
     baseURL: "https://herseymensformals.ml/"
+    // baseURL: "http://localhost:3500/"
+
 
 })
 
