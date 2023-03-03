76a884d42ffbedbaf981d063cdcecd163271db99
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-03-03 07:10:45 +0530

    updated

--- src/Client/VendorDetails/VendorDetails.jsx
+++ src/Client/VendorDetails/VendorDetails.jsx
@@ -4,7 +4,7 @@ import logo from '../../assets/img/Logo1.png'
 import Rating from '@mui/material/Rating';
 import { useDispatch } from 'react-redux'
 import { setModalOn } from '../../Redux/Reducer/vendorReviewModal'
-import VendorReviewModal from '../VendorReviewModal/vendorReviewModal'
+import VendorReviewModal from '../VendorReviewModal/VendorReviewModal'
 import Navbar from '../Home/Navbar'
 import { Link, useLocation } from 'react-router-dom';
 import { vendorReview, viewGigVendor } from '../../API';
